512e38e0fd5529d67df24cc0dcadf00fc85927d2
rlatkddn212 <rlatkddn212@naver.com>
2024-07-29 02:05:02 +0900

    동료 이동 애니메이션 적용 동료 AI 명령 진행중. ㄴ 상태 코드 컴포넌트로 이동, 전투 시 이동 처리가 완료되지 않음. 추가 애니메이션 리소스(소서로용) 커밋.

--- Source/HotDogma/HD_GameModeBase/CHJ_GameMode.cpp
+++ Source/HotDogma/HD_GameModeBase/CHJ_GameMode.cpp
@@ -2,4 +2,47 @@
 
 
 #include "../HD_GameModeBase/CHJ_GameMode.h"
+#include "HotDogma/Ksw/Companions/HD_CompanionManager.h"
+#include "HotDogma/LHJ/HD_Dragon.h"
+#include "EngineUtils.h"
 
+void ACHJ_GameMode::BeginPlay()
+{
+	if (bCompanionsSpawn)
+	{
+		CompanionManager = GetWorld()->SpawnActor<AHD_CompanionManager>(CompanionManagerFactory);
+	}
+
+	// 드래곤을 찾아둔다.
+	for (TActorIterator<AHD_Dragon> ActorItr(GetWorld()); ActorItr; ++ActorItr)
+	{
+		Dragons.Add(*ActorItr);
+	}
+}
+
+void ACHJ_GameMode::CommandCompanion(int num)
+{
+	UE_LOG(LogTemp, Warning, TEXT("CommandCompanion"));
+
+	// 동료 캐릭터에게 명령을 내린다.
+	if (CompanionManager) CompanionManager->SetCommand(ECompanionCommand(num));
+}
+
+APawn* ACHJ_GameMode::GetEnemy(FVector Pos)
+{
+		// 가장 가까운 드래곤을 찾는다.
+	APawn* ClosestDragon = nullptr;
+	float MinDistance = 9999999999.0f;
+
+	for (auto Dragon : Dragons)
+	{
+		float Distance = FVector::Dist(Dragon->GetActorLocation(), Pos);
+		if (Distance < MinDistance)
+		{
+			MinDistance = Distance;
+			ClosestDragon = Dragon;
+		}
+	}
+
+	return ClosestDragon;
+}
---
+++ Source/HotDogma/Ksw/Companions/HD_SorcererAnimInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "HD_SorcererAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOTDOGMA_API UHD_SorcererAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};

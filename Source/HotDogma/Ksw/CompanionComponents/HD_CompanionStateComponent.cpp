360ab7db1d65f93f6e996bf1b6ee68fa26c33dc5
rlatkddn212 <rlatkddn212@naver.com>
2024-07-29 21:09:00 +0900

    동료 캐릭터 액터에서 상태처리하던걸 컴포넌트로 이동

--- Source/HotDogma/Ksw/CompanionComponents/HD_CompanionStateComponent.cpp
+++ Source/HotDogma/Ksw/CompanionComponents/HD_CompanionStateComponent.cpp
@@ -2,6 +2,10 @@
 
 
 #include "HD_CompanionStateComponent.h"
+#include "DrawDebugHelpers.h"
+#include "HotDogma/Ksw/Companions/HD_CompanionAIController.h"
+#include "HotDogma/HD_GameModeBase/CHJ_GameMode.h"
+#include "HotDogma/Ksw/Companions/HD_CompanionCharacter.h"
 
 // Sets default values for this component's properties
 UHD_CompanionStateComponent::UHD_CompanionStateComponent()
@@ -19,8 +23,16 @@ void UHD_CompanionStateComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	// AI 컨트롤러를 생성하고 설정한다.
+	// 랜덤 백터 생성
+	APawn* pawn = Cast<APawn>(GetOwner());
+	if (pawn != nullptr)
+	{
+		AIController = Cast<AAIController>(pawn->GetController());
+	}
+
+	Acc = FVector(FMath::RandRange(-1, 1), FMath::RandRange(-1, 1), FMath::RandRange(-1, 1));
+	Acc.Normalize();
 }
 
 
@@ -29,6 +41,199 @@ void UHD_CompanionStateComponent::TickComponent(float DeltaTime, ELevelTick Tick
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (CurrentState == ECompanionState::State_Run)
+	{
+		float Distance = FVector::Dist(GetOwner()->GetActorLocation(), MovePoint);
+		if (Distance < 100)
+		{
+			SetState(ECompanionState::State_Wait);
+		}
+
+		ACHJ_GameMode* gameMode = Cast<ACHJ_GameMode>(GetWorld()->GetAuthGameMode());
+		if (gameMode != nullptr)
+		{
+			TargetPawn = gameMode->GetEnemy(GetOwner()->GetActorLocation());
+			if (TargetPawn != nullptr)
+			{
+				// 드래곤과의 거리를 계산한다.
+				float EnemyDistance = FVector::Dist(GetOwner()->GetActorLocation(), TargetPawn->GetActorLocation());
+				if (EnemyDistance < 2000)
+				{
+					SetState(ECompanionState::State_Battle);
+				}
+			}
+		}
+	}
+	else if (CurrentState == ECompanionState::State_Battle)
+	{
+		// 전투 상태에서는 드래곤을 공격한다.
+		if (AIController)
+		{
+			if (TargetPawn != nullptr)
+			{
+				// 드래곤과의 거리
+				float Distance = FVector::Dist(GetOwner()->GetActorLocation(), TargetPawn->GetActorLocation());
+				if (Distance > 200)
+				{
+					// 드래곤을 향해 이동한다.
+					AIController->MoveToActor(TargetPawn, 100.0f);
+				}
+				else
+				{
+					// 드래곤을 공격한다.
+					UE_LOG(LogTemp, Warning, TEXT("Attack"));
+				}
+			}
+		}
+	}
+
+	FString myState = UEnum::GetValueOrBitfieldAsString(CurrentState);
+	DrawDebugString(GetWorld(), GetOwner()->GetActorLocation(), myState, 0, FColor::Yellow, 0);
+}
+
+FVector UHD_CompanionStateComponent::Separation(const TArray<UHD_CompanionStateComponent*>& Boids)
+{
+	FVector Steer = FVector::ZeroVector;
+	int Count = 0;
+
+	for (UHD_CompanionStateComponent* Other : Boids)
+	{
+		if (Other == nullptr)
+		{
+			continue;
+		}
+		
+		float Distance = FVector::Dist(GetOwner()->GetActorLocation(), Other->GetOwner()->GetActorLocation());
+		if (0 < Distance && Distance < 1000)
+		{
+			FVector Diff = GetOwner()->GetActorLocation() - Other->GetOwner()->GetActorLocation();
+			Diff.Normalize();
+			Diff /= Distance;
+			Steer += Diff;
+			Count++;
+		}
+	}
+
+	if (Count > 0)
+	{
+		Steer /= Count;
+	}
+
+	return Steer;
+}
+
+FVector UHD_CompanionStateComponent::Alignment(const TArray<UHD_CompanionStateComponent*>& Boids)
+{
+	FVector Steer = FVector::ZeroVector;
+	int Count = 0;
+
+	for (UHD_CompanionStateComponent* Other : Boids)
+	{
+		if (Other == nullptr)
+		{
+			continue;
+		}
+
+		float Distance = FVector::Dist(GetOwner()->GetActorLocation(), Other->GetOwner()->GetActorLocation());
+		if (0 < Distance && Distance < 100)
+		{
+			Steer += Other->Acc;
+			Count++;
+		}
+	}
+
+	if (Count > 0)
+	{
+		Steer /= Count;
+	}
+
+	return Steer;
+}
+
+FVector UHD_CompanionStateComponent::Cohesion(const TArray<UHD_CompanionStateComponent*>& Boids)
+{
+	FVector Steer = FVector::ZeroVector;
+	int Count = 0;
+
+	for (UHD_CompanionStateComponent* Other : Boids)
+	{
+		if (Other == nullptr)
+		{
+			continue;
+		}
+		float Distance = FVector::Dist(GetOwner()->GetActorLocation(), Other->GetOwner()->GetActorLocation());
+		if (Distance > 0 && Distance < 100)
+		{
+			Steer += Other->GetOwner()->GetActorLocation();
+			Count++;
+		}
+	}
+
+	if (Count > 0)
+	{
+		Steer /= Count;
+		// steer로 이동하는 방향을 구한다.
+		Steer -= GetOwner()->GetActorLocation();
+	}
+
+	return Steer;
+}
+
+FVector UHD_CompanionStateComponent::FollowPlayer(const FVector& PlayerLocation)
+{
+	FVector Steer = PlayerLocation - GetOwner()->GetActorLocation();
+
+	return Steer;
+}
+
+void UHD_CompanionStateComponent::Flocking(const TArray<UHD_CompanionStateComponent*>& Boids, const FVector& PlayerLocation, float SeparationValue, float AlignmentValue, float CohesionValue)
+{
+	FVector Sep = Separation(Boids) * SeparationValue;
+	FVector Ali = Alignment(Boids) * AlignmentValue;
+	FVector Coh = Cohesion(Boids) * CohesionValue;
+	FVector Foll = FollowPlayer(PlayerLocation) * 0.2f;
+
+	Acc = Sep + Ali + Coh + Foll;
+	//Acc = Acc.GetClampedToMaxSize(1.0f);
+	//Acc.Normalize();
+}
+
+void UHD_CompanionStateComponent::SetMovePoint(const FVector& chacterPos, const FVector& Point)
+{
+	if (CurrentState == ECompanionState::State_Battle)
+	{
+		return;
+	}
+
+	SetState(ECompanionState::State_Run);
+	MovePoint = Point;
+	CharcterPoint = chacterPos;
+	if (AIController != nullptr)
+	{
+		AIController->MoveToLocation(MovePoint, -1.0f, true, true, true);
+	}
+}
+
+void UHD_CompanionStateComponent::StopMove()
+{
+	if (CurrentState == ECompanionState::State_Run)
+	{
+		if (AIController)
+			AIController->StopMovement();
+	}
+
+	SetState(ECompanionState::State_Wait);
+}
+
+void UHD_CompanionStateComponent::DoHelp()
+{
+
+	// 지원 스킬이 있는지 확인하고 사용한다.
+	// SetState(ECompanionState::State_Help);
+}
+
+void UHD_CompanionStateComponent::SetState(ECompanionState State)
+{
+	CurrentState = State;
 }
 
--- Source/HotDogma/Ksw/CompanionComponents/HD_CompanionStateComponent.h
+++ Source/HotDogma/Ksw/CompanionComponents/HD_CompanionStateComponent.h
@@ -6,13 +6,22 @@
 #include "Components/ActorComponent.h"
 #include "HD_CompanionStateComponent.generated.h"
 
+// Enum으로 상태 관리
+UENUM(BlueprintType)
+enum class ECompanionState : uint8
+{
+	State_Wait UMETA(DisplayName = "Wait"),
+	State_Run UMETA(DisplayName = "Run"),
+	State_Battle UMETA(DisplayName = "Battle"),
+	State_Help UMETA(DisplayName = "Help"),
+};
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class HOTDOGMA_API UHD_CompanionStateComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UHD_CompanionStateComponent();
 
@@ -20,9 +29,38 @@ protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+
+	// boids
+	// separation : 다른 개체들과의 거리를 유지
+	FVector Separation(const TArray<UHD_CompanionStateComponent*>& Boids);
+	// alignment : 다른 개체들과의 방향을 일치
+	FVector Alignment(const TArray<UHD_CompanionStateComponent*>& Boids);
+	// cohesion : 다른 개체들과의 거리를 유지하면서 그룹의 중심을 향함
+	FVector Cohesion(const TArray<UHD_CompanionStateComponent*>& Boids);
+	// follow player : 플레이어를 따라감
+	FVector FollowPlayer(const FVector& PlayerLocation);
+
+	void Flocking(const TArray<UHD_CompanionStateComponent*>& Boids, const FVector& PlayerLocation, float SeparationValue, float AlignmentValue, float CohesionValue);
+
+	void SetMovePoint(const FVector& chacterPos, const FVector& Point);
+	void StopMove();
+	void DoHelp();
+
+	void SetState(ECompanionState State);
+
+	FVector MovePoint;
+	FVector CharcterPoint;
+	ECompanionState CurrentState;
+
+	UPROPERTY()
+	class AAIController* AIController;
+
+	UPROPERTY()
+	class APawn* TargetPawn;
+
+	FVector Acc;
 };

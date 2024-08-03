f4b9e53bf654effedc36467018222f87abc7bd12
rlatkddn212 <rlatkddn212@naver.com>
2024-08-03 11:49:58 +0900

    공격 개발 시작 (개발 진행중)

--- Source/HotDogma/Ksw/Companions/HD_WarriorAnimInstance.cpp
+++ Source/HotDogma/Ksw/Companions/HD_WarriorAnimInstance.cpp
@@ -3,3 +3,17 @@
 
 #include "HD_WarriorAnimInstance.h"
 
+void UHD_WarriorAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+}
+
+
+void UHD_WarriorAnimInstance::PlayAttackMontage(int combo)
+{
+	if (AttackMontage)
+	{
+		FName MontageSection = FName(*FString::Printf(TEXT("Attack_%d"), combo));
+		Montage_Play(AttackMontage, 1.f, EMontagePlayReturnType::MontageLength, 0.f, true);
+	}
+}

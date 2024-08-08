1d7c780069db927199d514c3901059a63960ef0d
carttman <93562297+carttman@users.noreply.github.com>
2024-08-08 21:21:23 +0900

    UI 제작중, 인게임 UI 체력 연동

---
+++ Source/HotDogma/UI/HD_GameOverWidget.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "../UI/HD_GameOverWidget.h"
+
+void UHD_GameOverWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+}
+
+void UHD_GameOverWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	Super::NativeTick(MyGeometry, InDeltaTime);
+	
+}

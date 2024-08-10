7a41202d533fdf47e98b6f37dfaba83410707a8f
carttman <93562297+carttman@users.noreply.github.com>
2024-08-10 20:10:12 +0900

    게임클리어 베이스 위젯 생성

---
+++ Source/HotDogma/UI/HD_GameClearWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HD_GameClearWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOTDOGMA_API UHD_GameClearWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
